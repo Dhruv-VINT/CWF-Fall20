5a0a6242ff14e2e14d7778be81fa670541d5faf9
Dhruv-VINT <dhruv.rathore99@gmail.com>
2020-10-22 13:18:40 +0530

    testing

--- Prototype/Bot.py
+++ Prototype/Bot.py
@@ -1,11 +1,38 @@
 import discord
+import random
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = '!',intents = intents)
 
 @client.event
 async def on_ready():
-    print("bot is ready")
+    print("Bot is ready")
 
-client.run('NzY2MDM4MzQ3MDQ2MDYwMDYy.X4dizg.U2Sj3b6A5pvRsGH9T0XHrN-4IEA')
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined a server.")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left a server. ")
+
+@client.command(aliases=['hey','HEY'])
+async def Hey(ctx, *, question):
+    responses = ['These are just feelings. They’ll go away.',
+                 'You can’t just give up! Is that what a dinosaur would do?',
+                 'I’m curvy and I like it.',
+                 'Joey doesn’t share food!',
+                 'I don’t like it when people take food off my plate, okay?',
+                 'Hey, how you doin ?'
+                 'What am I gonna do? Keep trying to get rid of these feelings.',
+                 'Now, let’s go, baby. It’s food time. Bring it, bitch.',
+                 'Everything’s fine, it’s just a little crush.']
+    await ctx.send(f'{random.choice(responses)}')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit = amount)
+
+client.run('')
 
