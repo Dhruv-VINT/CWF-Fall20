8027bb332d6c90627265f09542b4863051f789fe
Dhruv-VINT <dhruv.rathore99@gmail.com>
2020-10-22 13:59:08 +0530

    testing

--- Prototype/Bot.py
+++ Prototype/Bot.py
@@ -6,19 +6,19 @@ intents = discord.Intents(messages = True, guilds = True, reactions = True, memb
 client = commands.Bot(command_prefix = '!',intents = intents)
 
 @client.event
-async def on_ready():
+async def on_ready(): # BOT IS ONLINE
     print("Bot is ready")
 
 @client.event
-async def on_member_join(member):
+async def on_member_join(member):  # NOTIFY WHEN A MEMBER JOINS THE SERVER
     print(f"{member} has joined a server.")
 
 @client.event
-async def on_member_remove(member):
+async def on_member_remove(member):  # NOTIFY WHEN A MEMBER LEAVES THE SERVER
     print(f"{member} has left a server. ")
 
 @client.command(aliases=['hey','HEY'])
-async def Hey(ctx, *, question):
+async def Hey(ctx, *, question):    # TALKING TO BOT via COMMANDS
     responses = ['These are just feelings. They’ll go away.',
                  'You can’t just give up! Is that what a dinosaur would do?',
                  'I’m curvy and I like it.',
@@ -31,8 +31,30 @@ async def Hey(ctx, *, question):
     await ctx.send(f'{random.choice(responses)}')
 
 @client.command()
-async def clear(ctx, amount=5):
+async def clear(ctx, amount=5):   # CLEAR RECENT MESSAGES ON THE CHANNEL
     await ctx.channel.purge(limit = amount)
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):  # KICK A MEMBER
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):  # BAN A MEMBER FROM SERVER
+    await member.ban(reason=reason)
+    await ctx.command(f'Banned {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 client.run('')
 
